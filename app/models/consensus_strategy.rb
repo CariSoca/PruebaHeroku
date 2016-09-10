f3b1abda05f24f11c464d12e49ef9ae057c7100c
Damian <dmianp6@gmail.com>
2016-09-10 18:11:33 -0300

    Arreglados algunos comentarios, tambien agregue resources en el archivo de routing para los controladores que tenemos por ahora

--- app/models/consensus_strategy.rb
+++ app/models/consensus_strategy.rb
@@ -1,12 +1,15 @@
 class ConsensusStrategy
   # Receives a hash with participants and their estimations.
   # Returns a positive number when consensus is achieved,
-  # 	else returns a negative number; must think some sort of code system
-  #   to identify for each possible conflict, the appropriate resolution strategy
+  # 	else returns a negative number.
+  # Must think some sort of code system
+  #   to identify for each possible conflict, 
+  #   the appropriate resolution strategy
   def applyConsensus (estimations)
   end	
 
   private
+  	# controls the argument received is hash type
     def parameterControl (param)
   	  param.is_a?(Hash) ? return true : return false
     end	
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
 	has_many :estimation_sessions
   has_and_belongs_to_many :users
   has_one :user
+
+  # link to current consensus strategy
+  attr_accessor :consensusStrategy
+  
 end
