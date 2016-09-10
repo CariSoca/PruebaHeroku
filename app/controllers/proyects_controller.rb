5cc0e04920077e9d392b803fa7eacfac49aa4a1b
Damian <dmianp6@gmail.com>
2016-09-10 16:39:17 -0300

    Commit anterior le falto borrar archivos viejos generados con nombre de modelo incorrecto

--- app/controllers/proyects_controller.rb
+++
@@ -1,2 +0,0 @@
-class ProyectsController < ApplicationController
-end
--- test/controllers/proyects_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProyectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
