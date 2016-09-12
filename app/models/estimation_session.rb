aa7d647f13b298b08cee5623fd1e9a74f084916c
Damian <dmianp6@gmail.com>
2016-09-12 12:47:11 -0300

    Fixes y funcionalidades agregadas
    -Arreglada funcion de promedio y testeada. Ahora retorna el resultado en
    decimal
    -Corregidos errores sintacticos
    -Corregidos errores de estilo marcados por el cliente
    Historia #20
    -Implementado metodo de asignacion automatica de estrategia de manera
    transparente (ver config/custom/strategies_ids.yml)
    -Falta implementar operacion en controlador, cuando owner da next o
    todos los participantes estimaron.
    La asignacion se hace en tiempo de resolucion de conflicto, en funcion
    del id de estrategia previamente seteado.
    Agregada tambien Historia #65
    -Implementado metodo de asignacion automatica de estrategia de manera
    transparente (ver config/custom/strategies_ids.yml)
    La asignacion se hace en tiempo de resolucion de conflicto, en funcion
    del id de estrategia previamente seteado.

--- app/models/estimation_session.rb
+++ app/models/estimation_session.rb
@@ -1,4 +1,32 @@
+require 'yaml'
+
 class EstimationSession < ApplicationRecord
   has_one :project
   has_many :stories
+
+  attr_accessor :consensus_strategy 
+
+  def calculate_estimation (story_id)
+  	set_consensus_strategy
+
+  	# Generate 'estimations' hash with participants estimations
+
+  	story = self.stories.find(story_id)
+  	partial_estimations = story.partial_estimations.all
+  	estimations = {}
+  	partial_estimations.each do |e|
+  		key = e.user.nickname
+  		estimations[:key] = e.estimation
+  	end	
+
+  	@consensus_strategy.apply_consensus(estimations)
+
+  end	
+
+  private
+  	def set_consensus_strategy
+  		strategies_ids = YAML.load_file("#{Rails.root.to_s}/config/custom/strategies_ids.yml")
+  		@consensus_strategy = Object::const_get(strategies_ids[self.strategyId]).new
+  	end	
+  	
 end
