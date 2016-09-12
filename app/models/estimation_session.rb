0533bce0008b7977359ee355fd92b9602a9f3997
fedealdunate <fedealduc94@gmail.com>
2016-09-12 17:49:56 -0300

    Al crear una sesion de estimacion se le asigna un string hex aleatorio de 32 bits

--- app/models/estimation_session.rb
+++ app/models/estimation_session.rb
@@ -1,4 +1,19 @@
+require 'securerandom'
+
 class EstimationSession < ApplicationRecord
   has_one :project
   has_many :stories
+  before_create :generate_link
+
+  
+  private
+
+  def generate_link
+  	self.sharedLink = loop do
+  		random_token = SecureRandom.hex(32)
+  		break random_token unless EstimationSession.exists?(sharedLink: random_token)
+  	end
+  end
+
+
 end
