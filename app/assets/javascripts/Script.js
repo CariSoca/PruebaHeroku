c6cc5bd52bdc2eb256165960439e3e492139e345
Nicolas Fabre <nicofh@hotmail.com>
2016-09-15 23:24:07 -0300

    agregarHistoria

---
+++ app/assets/javascripts/Script.js
@@ -0,0 +1,32 @@
+/// <reference path="angular.js" />
+
+var app = angular
+			.module("mainScreenModulo", [])
+			.controller("mainScreenControlador", function($scope) {
+				$scope.proyecto = {
+					name: "Proyecto",
+					estrategia: "PlanningPoker",
+					serie: "Fibonacci",
+					consenso: "Average"
+				};
+
+				$scope.estrategias = [{id: 1, name:"PlanningPoker"}];
+				$scope.idEstrategiaSeleccionada = 1;
+
+				$scope.series = [{id: 1, name:"Fibonacci"}];
+				$scope.idSerieSeleccionada = 1;
+
+				$scope.consensos =[{id: 1, name: "Average"}];
+				$scope.idConsensoSeleccionado = 1;
+
+				$scope.sync = true;
+
+				$scope.historias = ["login google","pull trello","pull jira"];
+
+				$scope.agregarHistoria = function () {
+					$scope.historias.push($scope.nuevaHistoria);
+					$scope.nuevaHistoria = null;
+				};
+
+
+			});
