d05262fc863172cc12a9f6e8fc34233ca19b0ce8
nicolura <n.d.luraschi@gmail.com>
2016-09-17 18:17:45 -0300

    Gestion datos de proyecto en vista

--- app/assets/javascripts/Script.js
+++ app/assets/javascripts/Script.js
@@ -19,18 +19,24 @@ var app = angular.module("mainScreenModule", []);
 				};
 
         $http.get("home.json").then(function(response) {      			  
-      			    $scope.projectName = response.data.name;  				  
-  				  	$scope.series = response.data.series;  				  
-  				  	$scope.strategies = response.data.consensusStrategies;
+      			    $scope.projectName = "New Project";  				  
+  				  	$scope.defaultSerie = response.data.default.serie;  				  
+  				  	$scope.selectedStrategyId = response.data.default.consensus;
   				  	$scope.consensusList = response.data.consensus_list;
-      			    $scope.seriesList = response.data.series_list;  				  	
+      			    $scope.seriesList = response.data.series_list;  
+      			    $scope.timebox = response.data.default.timebox;	
+      			    $scope.time = $scope.timebox[0] + ":" + $scope.timebox[1];
+      			    $scope.number_of_vals = response.data.default.number_of_vals;
+                    $scope.values = Array.apply(null, {length: $scope.number_of_vals + 1}).map(Number.call, Number)
+                    $scope.values.shift();			  	
+                    $scope.selectedQuantityValues = 1; 
   				}); 
 
 				$scope.sendData = function(){
 						var project = { name: $scope.projectName,
 										consensusStrategy : $scope.selectedStrategyId,
 										values : $scope.selectedQuantityValues,
-										serie : $scope.selectedSerieId,
+										serie : $scope.defaultSerie,
 										stories : $scope.stories														
 		          		};
 						$http.post("projects/", angular.toJson(project));					
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,11 @@ class HomeController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: {default:{consensus: "Average",
-					   series:"Fibonacci",
+					   serie:"Fibonacci",
 					   number_of_vals: 7,
 					   timebox: [1,0]},
-				  consensus_list: ["Average"],
-				  series_list:["Fibonacci","T-shirts","Powers of 2"] } }
+				     consensus_list: ["Average"],
+				     series_list:["Fibonacci","T-shirts","Powers of 2"] } }
     end
   end
   
